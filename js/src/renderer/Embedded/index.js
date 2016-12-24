372189d862b4cba9b3a47fe9f402b4264150159b
jpuri <jyotipuri@gmail.com>
2016-12-24 17:25:52 +0530

    rename attribute link to src for embedded links

--- js/src/renderer/Embedded/index.js
+++ js/src/renderer/Embedded/index.js
@@ -3,8 +3,8 @@ import { Entity } from 'draft-js';
 
 const Image = ({ block }) => {
   const entity = Entity.get(block.getEntityAt(0));
-  const { link, height, width } = entity.getData();
-  return (<iframe height={height} width={width} src={link} frameBorder="0" allowFullScreen />);
+  const { src, height, width } = entity.getData();
+  return (<iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen />);
 };
 
 Image.propTypes = {
