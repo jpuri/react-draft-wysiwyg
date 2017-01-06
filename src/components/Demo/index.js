b844dcf6d3bc430aecc122fc30536fd1e168de18
jpuri <jyotipuri@gmail.com>
2017-01-06 19:40:48 +0530

    latest docs build

--- src/components/Demo/index.js
+++ src/components/Demo/index.js
@@ -176,7 +176,7 @@ export default class Demo extends Component {
             toolbarClassName="demo-toolbar-absolute-high"
             wrapperClassName="demo-wrapper-relative-long"
             editorClassName="demo-editor-embedded"
-            contentState={{ "entityMap":{"0":{"type":"EMBEDDED_LINK","mutability":"MUTABLE","data":{"link":"https://www.youtube.com/embed/VbXNmIvWa1c","height":"auto","width":"100%"}}},"blocks":[{"key":"4vla1","text":"Demo of embedded links, this work so awesome with DraftJS:","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"1gls3","text":" ","type":"atomic","depth":0,"inlineStyleRanges":[],"entityRanges":[{"offset":0,"length":1,"key":0}],"data":{}},{"key":"4m681","text":"This is cool. Check by typing more here ...","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}] }}
+            contentState={{ "entityMap":{"0":{"type":"EMBEDDED_LINK","mutability":"MUTABLE","data":{"src":"https://www.youtube.com/embed/VbXNmIvWa1c","height":"auto","width":"100%"}}},"blocks":[{"key":"4vla1","text":"Demo of embedded links, this work so awesome with DraftJS:","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"1gls3","text":" ","type":"atomic","depth":0,"inlineStyleRanges":[],"entityRanges":[{"offset":0,"length":1,"key":0}],"data":{}},{"key":"4m681","text":"This is cool. Check by typing more here ...","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}] }}
             toolbarOnFocus
             toolbar={{
               inline: { inDropdown: true },
