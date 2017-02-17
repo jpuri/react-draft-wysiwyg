64f85c2d88740a51a67ba5ccb214bee7820f9090
Amitom <thomas.cochet@gmail.com>
2017-02-17 10:53:58 -0500

    Add translations for Image and FontFamily controls

---
+++ js/src/i18n/en.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+
+  // Image
+  'components.controls.image.fileUpload' : "File Upload",
+  'components.controls.image.byURL' : "URL",
+  'components.controls.image.dropFileText' : "Drop the file or click to upload",
+}
