d947db096377bd3349872e9a7be31e52984e16ad
Nicolas Lavoie Drapeau (lavoiedn) <lavoiedn@gmail.com>
2017-03-16 10:11:28 -0400

    Attempted a fix, error is no longer thrown but the image disappears

--- js/src/components/Controls/Image/index.js
+++ js/src/components/Controls/Image/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import React, { Component, PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Entity, AtomicBlockUtils } from 'draft-js';
 import classNames from 'classnames';
 import Option from '../../Option';
--- js/src/renderer/index.js
+++ js/src/renderer/index.js
@@ -1,5 +1,6 @@
 import Embedded from './Embedded';
-import getImageComponent from './Image';
+import getImageComponent from '../Renderer/Image';
+import { EditorState, SelectionState } from 'draft-js';
 
 const getBlockRenderFunc = (config, customBlockRenderer, getEditorState) => {
   return (block) => {
@@ -9,17 +10,31 @@ const getBlockRenderFunc = (config, customBlockRenderer, getEditorState) => {
     }
     if (block.getType() === 'atomic') {
       const contentState = getEditorState().getCurrentContent();
-      const entity = contentState.getEntity(block.getEntityAt(0));
-      if (entity && entity.type === 'IMAGE') {
-        return {
-          component: getImageComponent(config),
-          editable: false,
-        };
-      } else if (entity && entity.type === 'EMBEDDED_LINK') {
-        return {
-          component: Embedded,
-          editable: false,
-        };
+      let entityKey = block.getEntityAt(0);
+
+      if (entityKey) {
+        const entity = contentState.getEntity(entityKey);
+        if (entity && entity.type === 'IMAGE') {
+          console.log('image block selected', block.getKey(), contentState.getKeyAfter(block.getKey()))
+          // Force selection to leave image block to avoid errors
+          const nextBlockKey = contentState.getKeyAfter(block.getKey());
+          const selectionState = SelectionState.createEmpty(nextBlockKey);
+          selectionState.merge({
+            focusKey: nextBlockKey,
+            focusOffset: 0,
+            hasFocus: true
+          });
+          EditorState.forceSelection(getEditorState(), selectionState);
+          return {
+            component: getImageComponent(config),
+            editable: false,
+          };
+        } else if (entity && entity.type === 'EMBEDDED_LINK') {
+          return {
+            component: Embedded,
+            editable: false,
+          };
+        }
       }
     }
     return undefined;
