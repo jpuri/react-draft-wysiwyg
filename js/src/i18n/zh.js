33039a2303b4770abf12beff0c2a93b299d94af2
bang <sqibang@gmail.com>
2017-03-17 09:14:03 +0700

    feat: support chinese

---
+++ js/src/i18n/zh.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+  // Generic
+  'generic.add' : "添加",
+  'generic.cancel' : "取消",
+
+  // BlockType
+  'components.controls.blocktype.h1' : "H1",
+  'components.controls.blocktype.h2' : "H2",
+  'components.controls.blocktype.h3' : "H3",
+  'components.controls.blocktype.h4' : "H4",
+  'components.controls.blocktype.h5' : "H5",
+  'components.controls.blocktype.h6' : "H6",
+  'components.controls.blocktype.blockquote' : "引用",
+  'components.controls.blocktype.blocktype' : "块类型",
+  'components.controls.blocktype.normal' : "常规",
+
+  // Color Picker
+  'components.controls.colorpicker.text' : "文本",
+  'components.controls.colorpicker.background' : "背景",
+
+  // Embedded
+  'components.controls.embedded.embeddedlink' : "嵌入链接",
+  'components.controls.embedded.enterlink' : "链接地址",
+
+  // FontFamily
+  'components.controls.fontfamily.fontfamily' : "字体",
+
+  // Image
+  'components.controls.image.fileUpload' : "图片上传",
+  'components.controls.image.byURL' : "图片地址",
+  'components.controls.image.dropFileText' : "拖到这里或点击上传",
+
+  //Link
+  'components.controls.link.linkTitle' : "链接标题",
+  'components.controls.link.linkTarget' : "链接地址",
+}
