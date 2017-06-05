1907600e8ab620b229f0b054001d405d93ad0d27
Sunny Golovine <sunny@golovinemail.org>
2017-06-05 12:11:48 -0400

    changed upload URL string

--- js/src/i18n/en.js
+++ js/src/i18n/en.js
@@ -28,7 +28,7 @@ module.exports = {
 
   // Image
   'components.controls.image.fileUpload': 'File Upload',
-  'components.controls.image.byURL': 'URL Upload',
+  'components.controls.image.byURL': 'URL',
   'components.controls.image.dropFileText': 'Drop the file or click to upload',
 
   // Link
