749cb49f45bafe41bfe980103f5c2671cb63a1d7
zerocho <zerohch0@gmail.com>
2017-06-30 20:29:36 +0900

    i18n for Korean(ko)

--- docs/src/components/Docs/index.js
+++ docs/src/components/Docs/index.js
@@ -274,7 +274,7 @@ export default class Demo2 extends Component {
           </div>
           <div className="docs-desc">
             Toolbar labels can be internationalized using property localization. Localization has 2 parameters: locale and translations. <br/>
-            User can either provide locale or a JavaScript object for translations. Currently supported locales are: English('en'), French('fr'), Chinese('zh'), Russian('ru') and Portuguese('pt').<br/>
+            User can either provide locale or a JavaScript object for translations. Currently supported locales are: English('en'), French('fr'), Chinese('zh'), Russian('ru'), Portuguese('pt') and Korean('ko').<br/>
             For an example of translations check <a href="https://github.com/jpuri/react-draft-wysiwyg/blob/master/js/src/i18n/en.js">here</a>.
           </div>
         </div>
