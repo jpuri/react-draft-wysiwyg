4aa2ba8869746abc694b5ad7cce2a6ceec5b9258
jpuri <jyotipuri@gmail.com>
2017-07-02 20:03:55 +0530

    Fix import case issue - removing directories

--- js/src/decorators/Mention/index.js
+++
@@ -1,9 +0,0 @@
-import Mention from './Mention';
-import Suggestion from './Suggestion';
-
-const getDecorators = config => [
-  (new Mention(config.mentionClassName)).getMentionDecorator(),
-  (new Suggestion(config)).getSuggestionDecorator(),
-];
-
-module.exports = getDecorators;
