64efd5716f3748017047dcf7abf2d5abbfb801e6
jpuri <jyotipuri@gmail.com>
2017-07-12 20:53:30 +0530

    Adding more stories for reference

--- stories/ReadOnly/index.js
+++ stories/ReadOnly/index.js
@@ -5,8 +5,9 @@ import { Editor } from '../../src';
 
 const Basic = () =>
   (<div className="rdw-storybook-root">
-    <h3>Basic Editor</h3>
+    <h3>ReadOnly editor</h3>
     <Editor
+      readOnly
       toolbarClassName="rdw-storybook--toolbar"
       wrapperClassName="rdw-storybook-wrapper"
       editorClassName="rdw-storybook-editor"
