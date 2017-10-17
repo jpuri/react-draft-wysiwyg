ef2ee514be2a559d7e1466eb69fb13ec66b729da
Dan Petranek <dan.petranek@wicourts.gov>
2017-10-17 15:00:54 -0500

    Adjust alignment metadata for indentation persistence
    This allows indentation to survive EditorState->html->EditorState transitions
    with the updated version of htmlToDraftJs. The css is necessary to show the
    indentation 'live', while the metadata gets translated into an inline style.
    
    This is a companion to a change in htmlToDraftJs that translates an inline style
    back into block metadata.

--- src/utils/BlockStyle.js
+++ src/utils/BlockStyle.js
@@ -1,10 +1,10 @@
 // The function will return block inline styles using block level meta-data
 export default function blockStyleFn(block: Object): string {
   const blockAlignment = block.getData() && block.getData().get('text-align');
-  const blockDepth = block.getData() && block.getData().get('indent');
+  const blockDepth = block.getData() && block.getData().get('margin-left');
   if (blockAlignment) {
     return `rdw-${blockAlignment}-aligned-block`;
-  } else if (blockDepth || blockDepth === 0) {
+  } else if (blockDepth) {
     return `rdw-indent-${blockDepth}`;
   }
   return '';
