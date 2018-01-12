608ca89b7ec155f06f2e1f36816897c08a845246
Saul-Mirone <xy610888871@gmail.com>
2018-01-12 19:23:28 +0800

    optimise paste from source

--- src/utils/handlePaste.js
+++ src/utils/handlePaste.js
@@ -1,7 +1,7 @@
 import { getSelectedBlock } from 'draftjs-utils';
-import { Modifier, EditorState, ContentState } from 'draft-js';
-import htmlToDraft from 'html-to-draftjs';
-import { OrderedMap } from 'immutable';
+
+import { List, Repeat } from 'immutable';
+import { Modifier, CharacterMetadata, BlockMapBuilder, EditorState, ContentBlock, genKey } from 'draft-js';
 
 export const handlePastedText = (text, html, editorState, onChange) => {
   const selectedBlock = getSelectedBlock(editorState);
@@ -15,22 +15,69 @@ export const handlePastedText = (text, html, editorState, onChange) => {
     onChange(EditorState.push(editorState, contentState, 'insert-characters'));
     return true;
   } else if (html) {
-    const contentBlock = htmlToDraft(html);
-    let blockMap = new OrderedMap({});
-    contentBlock.contentBlocks.forEach(block => {
-      blockMap = blockMap.set(block.get('key'), block);
-    });
-    let contentState = editorState.getCurrentContent();
-    contentBlock.entityMap.forEach((value, key) => {
-      contentState = contentState.mergeEntityData(key, value);
-    });
-    contentState = Modifier.replaceWithFragment(
-      contentState,
-      editorState.getSelection(),
-      blockMap,
+    const currentContentState = editorState.getCurrentContent();
+    const currentSelectionState = editorState.getSelection();
+    const afterRemovalContentState = Modifier.removeRange(
+      currentContentState,
+      currentSelectionState,
+      'backward',
     );
-    onChange(EditorState.push(editorState, contentState, 'insert-characters'));
-    return true;
+    const targetSelection = afterRemovalContentState.getSelectionAfter();
+    const blockKeyForTarget = targetSelection.get('focusKey');
+    const block = currentContentState.getBlockForKey(blockKeyForTarget);
+    let insertionTargetSelection;
+    let insertionTargetBlock;
+    const isEmptyBlock = block.getLength() === 0 && block.getEntityAt(0) === null;
+    const selectedFromStart = currentSelectionState.getStartOffset() === 0;
+    if (isEmptyBlock || selectedFromStart) {
+      insertionTargetSelection = targetSelection;
+      insertionTargetBlock = afterRemovalContentState;
+    } else {
+      insertionTargetBlock = Modifier.splitBlock(afterRemovalContentState, targetSelection);
+
+      insertionTargetSelection = insertionTargetBlock.getSelectionAfter();
+    }
+    const entityKey = currentContentState.createEntity('sticker', 'IMMUTABLE', selectedBlock.data);
+    const charDataOfSticker = CharacterMetadata.create({ entity: entityKey });
+
+    const { type } = selectedBlock;
+
+    const fragmentArray = [
+      new ContentBlock({
+        key: genKey(),
+        type,
+        text: '',
+        characterList: List(Repeat(charDataOfSticker, 1)), // eslint-disable-line new-cap
+      }),
+
+      new ContentBlock({
+        key: genKey(),
+        type: 'unstyled',
+        text: '',
+        characterList: List(), // eslint-disable-line new-cap
+      }),
+    ];
+
+    const fragment = BlockMapBuilder.createFromArray(fragmentArray);
+    const newContentStateAfterSplit = Modifier
+      .setBlockType(insertionTargetBlock, insertionTargetSelection, type);
+
+    const contentStateWithSticker = Modifier.replaceWithFragment(
+      newContentStateAfterSplit,
+      insertionTargetSelection,
+      fragment,
+    );
+
+    const newState = EditorState.push(
+      editorState,
+      contentStateWithSticker,
+      'insert-sticker',
+    );
+    const afterInsert = EditorState
+      .forceSelection(newState, contentStateWithSticker.getSelectionAfter());
+
+    return onChange(afterInsert);
   }
   return false;
-}
+};
+
