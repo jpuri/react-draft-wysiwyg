1a214fdf1d6f8e27510820e586c541163cd1f818
Anton Neklesa <anek@herobase.com>
2018-06-12 19:45:53 +0300

    Add Swedish language

---
+++ src/i18n/se.js
@@ -0,0 +1,81 @@
+module.exports = {
+
+  // Generic
+  'generic.add': 'Lägg till',
+  'generic.cancel': 'Avbryt',
+
+  // BlockType
+  'components.controls.blocktype.h1': 'Rubrik 1',
+  'components.controls.blocktype.h2': 'Rubrik 2',
+  'components.controls.blocktype.h3': 'Rubrik 3',
+  'components.controls.blocktype.h4': 'Rubrik 4',
+  'components.controls.blocktype.h5': 'Rubrik 5',
+  'components.controls.blocktype.h6': 'Rubrik 6',
+  'components.controls.blocktype.blockquote': 'Citat',
+  'components.controls.blocktype.code': 'Kod',
+  'components.controls.blocktype.blocktype': 'Block Typ',
+  'components.controls.blocktype.normal': 'Normal',
+
+  // Color Picker
+  'components.controls.colorpicker.colorpicker': 'Färger',
+  'components.controls.colorpicker.text': 'Text',
+  'components.controls.colorpicker.background': 'Markering',
+
+  // Embedded
+  'components.controls.embedded.embedded': 'Integrerad',
+  'components.controls.embedded.embeddedlink': 'Integrerad länk',
+  'components.controls.embedded.enterlink': 'Lägg till länk',
+
+  // Emoji
+  'components.controls.emoji.emoji': 'Emoji',
+
+  // FontFamily
+  'components.controls.fontfamily.fontfamily': 'Typsnitt',
+
+  // FontSize
+  'components.controls.fontsize.fontsize': 'Punktstorlek',
+
+  // History
+  'components.controls.history.history': 'Historik',
+  'components.controls.history.undo': 'Ångra',
+  'components.controls.history.redo': 'Gör om',
+
+  // Image
+  'components.controls.image.image': 'bild',
+  'components.controls.image.fileUpload': 'Ladda upp',
+  'components.controls.image.byURL': 'URL',
+  'components.controls.image.dropFileText': 'Släpp filen här eller klicka "Ladda upp"',
+
+  // Inline
+  'components.controls.inline.bold': 'Fet',
+  'components.controls.inline.italic': 'Kursiv',
+  'components.controls.inline.underline': 'Understruken',
+  'components.controls.inline.strikethrough': 'Genomstruken',
+  'components.controls.inline.monospace': 'Fast bredd',
+  'components.controls.inline.superscript': 'Upphöjt',
+  'components.controls.inline.subscript': 'Nersänkt',
+
+  // Link
+  'components.controls.link.linkTitle': 'Länk Titel',
+  'components.controls.link.linkTarget': 'Länk mål',
+  'components.controls.link.linkTargetOption': 'Öppna länk i nytt fönster',
+  'components.controls.link.link': 'Länka',
+  'components.controls.link.unlink': 'Ta bort länkning',
+
+  // List
+  'components.controls.list.list': 'Lista',
+  'components.controls.list.unordered': 'Osorterad',
+  'components.controls.list.ordered': 'Sorterad',
+  'components.controls.list.indent': 'Indrag',
+  'components.controls.list.outdent': 'Utdrag',
+
+  // Remove
+  'components.controls.remove.remove': 'Ta bort',
+
+  // TextAlign
+  'components.controls.textalign.textalign': 'Text orientering',
+  'components.controls.textalign.left': 'Vänster',
+  'components.controls.textalign.center': 'Centrera',
+  'components.controls.textalign.right': 'Höger',
+  'components.controls.textalign.justify': 'Utjämna',
+};
