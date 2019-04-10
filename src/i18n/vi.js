22b0852bd4475f5f0fa44991705cc62952fd274f
admin <dinhhoainam94@gmail.com>
2019-04-10 09:49:29 +0700

    Add vietnamese language to Contributors list

---
+++ src/i18n/vi.js
@@ -0,0 +1,81 @@
+module.exports = {
+
+    // Generic
+    'generic.add': 'Thêm mới',
+    'generic.cancel': 'Hủy bỏ',
+
+    // BlockType
+    'components.controls.blocktype.h1': 'H1',
+    'components.controls.blocktype.h2': 'H2',
+    'components.controls.blocktype.h3': 'H3',
+    'components.controls.blocktype.h4': 'H4',
+    'components.controls.blocktype.h5': 'H5',
+    'components.controls.blocktype.h6': 'H6',
+    'components.controls.blocktype.blockquote': 'Blockquote',
+    'components.controls.blocktype.code': 'Code',
+    'components.controls.blocktype.blocktype': 'Block Type',
+    'components.controls.blocktype.normal': 'Normal',
+
+    // Color Picker
+    'components.controls.colorpicker.colorpicker': 'Bảng chọn màu',
+    'components.controls.colorpicker.text': 'Văn bản',
+    'components.controls.colorpicker.background': 'Highlight',
+
+    // Embedded
+    'components.controls.embedded.embedded': 'Embedded',
+    'components.controls.embedded.embeddedlink': 'Embedded Link',
+    'components.controls.embedded.enterlink': 'Enter link',
+
+    // Emoji
+    'components.controls.emoji.emoji': 'Biểu cảm',
+
+    // FontFamily
+    'components.controls.fontfamily.fontfamily': 'Font chữ',
+
+    // FontSize
+    'components.controls.fontsize.fontsize': 'Cỡ chữ',
+
+    // History
+    'components.controls.history.history': 'Lịch sử',
+    'components.controls.history.undo': 'Undo',
+    'components.controls.history.redo': 'Redo',
+
+    // Image
+    'components.controls.image.image': 'Ảnh',
+    'components.controls.image.fileUpload': 'Tải tệp lên',
+    'components.controls.image.byURL': 'Dán liên kết vào',
+    'components.controls.image.dropFileText': 'Thả tệp hay click để tải lên',
+
+    // Inline
+    'components.controls.inline.bold': 'In đậm',
+    'components.controls.inline.italic': 'In nghiêng',
+    'components.controls.inline.underline': 'Gạch chân',
+    'components.controls.inline.strikethrough': 'Gạch ngang',
+        'components.controls.inline.monospace': 'Khoảng cách',
+    'components.controls.inline.superscript': 'Chỉ số trên',
+    'components.controls.inline.subscript': 'Chỉ số dưới',
+
+    // Link
+    'components.controls.link.linkTitle': 'Tiêu đề liên kết',
+    'components.controls.link.linkTarget': 'Mục tiêu của liên kết',
+    'components.controls.link.linkTargetOption': 'Mở liên kết trong tab mới',
+    'components.controls.link.link': 'Liên kết',
+    'components.controls.link.unlink': 'Hủy liên kết',
+
+    // List
+    'components.controls.list.list': 'Danh sách',
+    'components.controls.list.unordered': 'Không sắp xếp',
+    'components.controls.list.ordered': 'Đã sắp xếp',
+    'components.controls.list.indent': 'Căn lề trong',
+    'components.controls.list.outdent': 'Căn lề ngoài',
+
+    // Remove
+    'components.controls.remove.remove': 'Loại bỏ',
+
+    // TextAlign
+    'components.controls.textalign.textalign': 'Căn chỉnh văn bản',
+    'components.controls.textalign.left': 'Trái',
+    'components.controls.textalign.center': 'Giữa',
+    'components.controls.textalign.right': 'Phải',
+    'components.controls.textalign.justify': 'Căn đều 2 bên',
+};
