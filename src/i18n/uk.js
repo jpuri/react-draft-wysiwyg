76065f80ed85717e4f83c152227e304cbc58a7ed
Sushasgit <gryshchukoleksandra@gmail.com>
2019-06-03 17:40:05 +0300

    Small translation fixes

--- src/i18n/uk.js
+++ src/i18n/uk.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     // Generic
-    'generic.add': 'Добавить',
-    'generic.cancel': 'Отменить',
-  
+    'generic.add': 'Додати',
+    'generic.cancel': 'Відмінити',
+
     // BlockType
     'components.controls.blocktype.h1': 'Заголовок 1',
     'components.controls.blocktype.h2': 'Заголовок 2',
@@ -15,63 +15,63 @@ module.exports = {
     'components.controls.blocktype.code': 'Код',
     'components.controls.blocktype.blocktype': 'Форматування',
     'components.controls.blocktype.normal': 'Звичайний',
-  
+
     // Color Picker
     'components.controls.colorpicker.colorpicker': 'Вибір кольору',
     'components.controls.colorpicker.text': 'Текст',
     'components.controls.colorpicker.background': 'Фон',
-  
+
     // Embedded
     'components.controls.embedded.embedded': 'Вбудовування',
     'components.controls.embedded.embeddedlink': 'Посилання в iFrame',
     'components.controls.embedded.enterlink': 'Вставте посилання',
-  
+
     // Emoji
     'components.controls.emoji.emoji': 'Емодзі',
-  
+
     // FontFamily
     'components.controls.fontfamily.fontfamily': 'Шрифт',
-  
+
     // FontSize
     'components.controls.fontsize.fontsize': 'Розмір шрифта',
-  
+
     // History
     'components.controls.history.history': 'Історія',
     'components.controls.history.undo': 'Скасувати',
     'components.controls.history.redo': 'Повернути',
-  
+
     // Image
     'components.controls.image.image': 'Зображення',
     'components.controls.image.fileUpload': 'Файли',
     'components.controls.image.byURL': 'URL',
-    'components.controls.image.dropFileText': 'Перемістіть в цю область файли або клікніть для завантаження',
-  
+    'components.controls.image.dropFileText': 'Перемістіть файли в цю область або клікніть для завантаження',
+
     // Inline
     'components.controls.inline.bold': 'Жирний',
     'components.controls.inline.italic': 'Курсив',
-    'components.controls.inline.underline': 'Підкреслення',
-    'components.controls.inline.strikethrough': 'Закреслення',
+    'components.controls.inline.underline': 'Підкресленний',
+    'components.controls.inline.strikethrough': 'Закресленний',
     'components.controls.inline.monospace': 'Monospace',
     'components.controls.inline.superscript': 'Верхній індекс',
     'components.controls.inline.subscript': 'Нижній індекс',
-  
+
     // Link
     'components.controls.link.linkTitle': 'Текст',
     'components.controls.link.linkTarget': 'Адреса посилання',
     'components.controls.link.linkTargetOption': 'Відкривати в новому вікні',
     'components.controls.link.link': 'Посилання',
     'components.controls.link.unlink': 'Прибрати посилання',
-  
+
     // List
     'components.controls.list.list': 'Список',
     'components.controls.list.unordered': 'Невпорядкований',
     'components.controls.list.ordered': 'Упорядкований',
     'components.controls.list.indent': 'Відступ',
     'components.controls.list.outdent': 'Виступ',
-  
+
     // Remove
     'components.controls.remove.remove': 'Видалити',
-  
+
     // TextAlign
     'components.controls.textalign.textalign': 'Вирівнювання тексту',
     'components.controls.textalign.left': 'Ліворуч',
@@ -79,4 +79,3 @@ module.exports = {
     'components.controls.textalign.right': 'Праворуч',
     'components.controls.textalign.justify': 'Вирівняти',
   };
-  
