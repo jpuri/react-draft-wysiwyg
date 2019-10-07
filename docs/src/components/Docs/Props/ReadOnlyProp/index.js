406e12f50709fb82ecb408f5a15fa70e0a84f040
Raphaël Badia <1901628+raphaelbadia@users.noreply.github.com>
2019-10-07 10:43:43 +0200

    Update index.js

--- docs/src/components/Docs/Props/ReadOnlyProp/index.js
+++ docs/src/components/Docs/Props/ReadOnlyProp/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default () => (
   <div>
-    <h3>ReadOnly</h3>
+    <h3>readOnly</h3>
     <div className="docs-desc">
-      The property can be used to render Editor in ReadOnly mode. 
+      The property can be used to render Editor in read only mode. 
       When its true user can not change editor text or use any toolbar option.
     </div>
   </div>
