b7df9b76168df568df29da0ca39f18398e4d11df
jpuri <jyotipuri@gmail.com>
2019-12-06 17:47:56 +0530

    Fixing build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,51 +1,51 @@
-const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require("autoprefixer");
-const precss = require("precss");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  devtool: "source-map",
-  entry: ["./src/index"],
+  devtool: 'source-map',
+  entry: ['./src/index'],
   output: {
-    path: path.join(__dirname, "../dist"),
-    filename: "react-draft-wysiwyg.js",
-    library: "reactDraftWysiwyg",
-    libraryTarget: "umd"
+    path: path.join(__dirname, '../dist'),
+    filename: 'react-draft-wysiwyg.js',
+    library: 'reactDraftWysiwyg',
+    libraryTarget: 'umd',
   },
   externals: {
-    react: "react",
-    immutable: "immutable",
-    "react-dom": "react-dom",
-    "draft-js": "draft-js"
+    react: 'react',
+    immutable: 'immutable',
+    'react-dom': 'react-dom',
+    'draft-js': 'draft-js',
   },
   optimization: {
-    minimizer: [new UglifyJsPlugin()]
+    minimizer: [new UglifyJsPlugin()],
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-      ignoreOrder: false
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
     }),
     new webpack.LoaderOptionsPlugin({
       options: {
-        postcss: [autoprefixer, precss]
-      }
-    })
+        postcss: [autoprefixer, precss],
+      },
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [{ loader: "babel-loader" }],
-        exclude: /immutable\.js$|draftjs-utils\.js$/
+        use: [{ loader: 'babel-loader' }],
+        exclude: /immutable\.js$|draftjs-utils\.js$/,
       },
       {
         test: /\.css$/,
@@ -53,21 +53,21 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: "../",
-              hmr: process.env.NODE_ENV === "development"
-            }
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
           },
-          "css-loader"
-        ]
+          'css-loader',
+        ],
       },
-      { test: /\.(png|jpg)$/, use: [{ loader: "url-loader?limit=8192" }] },
+      { test: /\.(png|jpg)$/, use: [{ loader: 'url-loader?limit=8192' }] },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{ loader: "url-loader?limit=10000&mimetype=image/svg+xml" }]
-      }
-    ]
+        use: [{ loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".json"]
-  }
+    extensions: ['.js', '.json'],
+  },
 };
