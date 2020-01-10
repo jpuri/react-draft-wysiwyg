b917617f913c2c908f126cf0a8262c1c72470fed
jpuri <jyotipuri@gmail.com>
2020-01-10 18:53:07 +0530

    Fix toolbar issue in controlled component.

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -75,8 +75,8 @@ class WysiwygEditor extends Component {
   componentDidUpdate(prevProps) {
     if (prevProps === this.props) return;
     const newState = {};
-    const { toolbar, editorState, contentState } = this.props;
-    if (toolbar !== prevProps.toolbar) {
+    const { editorState, contentState } = this.props;
+    if (!this.state.toolbar) {
       const toolbar = mergeRecursive(defaultToolbar, toolbar);
       newState.toolbar = toolbar;
     }
--- stories/SelectedOptions/index.js
+++ stories/SelectedOptions/index.js
@@ -3,20 +3,21 @@
 import React from 'react';
 import { Editor } from '../../src';
 
-const SelectedOptions = () =>
-(<div className="rdw-storybook-root">
-  <h3>Editor with only selected toolbar options.</h3>
-  <Editor
-    toolbar={{
-      options: ['inline', 'blockType', 'fontSize', 'fontFamily'],
-      inline: {
-        options: ['italic', 'underline'],
-      },
-    }}
-    toolbarClassName="rdw-storybook-toolbar"
-    wrapperClassName="rdw-storybook-wrapper"
-    editorClassName="rdw-storybook-editor"
-  />
-</div>);
+const SelectedOptions = () => (
+  <div className="rdw-storybook-root">
+    <h3>Editor with only selected toolbar options.</h3>
+    <Editor
+      wrapperClassName="demo-wrapper"
+      editorClassName="demo-editor"
+      toolbar={{
+        inline: { inDropdown: true },
+        list: { inDropdown: true },
+        textAlign: { inDropdown: true },
+        link: { inDropdown: true },
+        history: { inDropdown: true },
+      }}
+    />
+  </div>
+);
 
 export default SelectedOptions;
