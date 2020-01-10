6a6537acadee656cff42c2f363858a877d3f2290
jpuri <jyotipuri@gmail.com>
2020-01-10 18:53:07 +0530

    Fix toolbar issue in controlled component.

--- src/components/Demo/EditorImage/index.js
+++ src/components/Demo/EditorImage/index.js
@@ -7,40 +7,43 @@ import uploadImageCallBack from '../../../util/uploadImageCallBack';
 
 const EditorImage = () => (
   <div className="demo-section">
-    <h3>10. Editor with image upload option, mandatory alt field and image preview enabled.</h3>
+    <h3>
+      10. Editor with image upload option, mandatory alt field and image preview
+      enabled.
+    </h3>
     <div className="demo-section-wrapper">
       <div className="demo-editor-wrapper">
         <Editor
-            wrapperClassName="demo-wrapper"
-            editorClassName="demo-editor"
-            toolbar={{
-              options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'link', 'embedded', 'image', 'history'],
-              inline: { inDropdown: true },
-              link: { inDropdown: true },
-              history: { inDropdown: true },
-              image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true }, previewImage: true },
-            }}
-          />
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+          }}
+        />
       </div>
       <Codemirror
         value={
-          'import React from \'react\';\n' +
-          'import { Editor } from \'react-draft-wysiwyg\';\n' +
+          "import React from 'react';\n" +
+          "import { Editor } from 'react-draft-wysiwyg';\n" +
           '\n\n' +
           'function uploadImageCallBack(file) {\n' +
           '  return new Promise(\n' +
           '    (resolve, reject) => {\n' +
           '      const xhr = new XMLHttpRequest();\n' +
-          '      xhr.open(\'POST\', \'https://api.imgur.com/3/image\');\n' +
-          '      xhr.setRequestHeader(\'Authorization\', \'Client-ID XXXXX\');\n' +
+          "      xhr.open('POST', 'https://api.imgur.com/3/image');\n" +
+          "      xhr.setRequestHeader('Authorization', 'Client-ID XXXXX');\n" +
           '      const data = new FormData();\n' +
-          '      data.append(\'image\', file);\n' +
+          "      data.append('image', file);\n" +
           '      xhr.send(data);\n' +
-          '      xhr.addEventListener(\'load\', () => {\n' +
+          "      xhr.addEventListener('load', () => {\n" +
           '        const response = JSON.parse(xhr.responseText);\n' +
           '        resolve(response);\n' +
           '      });\n' +
-          '      xhr.addEventListener(\'error\', () => {\n' +
+          "      xhr.addEventListener('error', () => {\n" +
           '        const error = JSON.parse(xhr.responseText);\n' +
           '        reject(error);\n' +
           '      });\n' +
