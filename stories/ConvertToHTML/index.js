c2888098fd1009d648733e59343bf55ce8094dd7
Riya Lohia <riyalh1997@gmail.com>
2020-11-25 18:34:53 +0530

    chore: updates version

--- stories/ConvertToHTML/index.js
+++ stories/ConvertToHTML/index.js
@@ -20,30 +20,11 @@ class ConvertToRawDraftContent extends Component {
     const { editorState } = this.state;
     return (<div className="rdw-storybook-root">
       <Editor
-        mention={{
-          separator: ' ',
-          trigger: '@',
-          suggestions: [
-            { text: 'APPLE', value: 'apple', url: 'apple' },
-            { text: 'BANANA', value: 'banana', url: 'banana' },
-            { text: 'CHERRY', value: 'cherry', url: 'cherry' },
-            { text: 'DURIAN', value: 'durian', url: 'durian' },
-            { text: 'EGGFRUIT', value: 'eggfruit', url: 'eggfruit' },
-            { text: 'FIG', value: 'fig', url: 'fig' },
-            { text: 'GRAPEFRUIT', value: 'grapefruit', url: 'grapefruit' },
-            { text: 'HONEYDEW', value: 'honeydew', url: 'honeydew' },
-          ],
-        }}
         editorState={editorState}
         toolbarClassName="rdw-storybook-toolbar"
         wrapperClassName="rdw-storybook-wrapper"
         editorClassName="rdw-storybook-editor"
         onEditorStateChange={this.onEditorStateChange}
-        toolbar={{
-          link: {
-            defaultTargetOption: '_blank',
-          },
-        }}
       />
       <textarea
         readOnly
