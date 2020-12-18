624570ee4efb5acec93f9f22adad9b8832471b1a
Bugra <ordabiadamvar@gmail.com>
2020-12-18 18:01:12 +0300

    Turkish translation
    Turkish translations added, please accept this so i can use this plugin in my website.

---
+++ src/i18n/tr.js
@@ -0,0 +1,81 @@
+module.exports = {
+
+  // Generic
+  'generic.add': 'Ekle',
+  'generic.cancel': 'İptal Et',
+
+  // BlockType
+  'components.controls.blocktype.h1': 'H1',
+  'components.controls.blocktype.h2': 'H2',
+  'components.controls.blocktype.h3': 'H3',
+  'components.controls.blocktype.h4': 'H4',
+  'components.controls.blocktype.h5': 'H5',
+  'components.controls.blocktype.h6': 'H6',
+  'components.controls.blocktype.blockquote': 'Alıntı',
+  'components.controls.blocktype.code': 'Kod',
+  'components.controls.blocktype.blocktype': 'Blok Tipi',
+  'components.controls.blocktype.normal': 'Normal',
+
+  // Color Picker
+  'components.controls.colorpicker.colorpicker': 'Renk Seçici',
+  'components.controls.colorpicker.text': 'Yazı',
+  'components.controls.colorpicker.background': 'Vurgula',
+
+  // Embedded
+  'components.controls.embedded.embedded': 'Gömülü',
+  'components.controls.embedded.embeddedlink': 'Gömülü Link',
+  'components.controls.embedded.enterlink': 'Link Girin',
+
+  // Emoji
+  'components.controls.emoji.emoji': 'Emoji',
+
+  // FontFamily
+  'components.controls.fontfamily.fontfamily': 'Yazı Stili',
+
+  // FontSize
+  'components.controls.fontsize.fontsize': 'Yazı Boyutu',
+
+  // History
+  'components.controls.history.history': 'Geçmiş',
+  'components.controls.history.undo': 'Geri Al',
+  'components.controls.history.redo': 'İleri Al',
+
+  // Image
+  'components.controls.image.image': 'Resim',
+  'components.controls.image.fileUpload': 'Dosya Yükleme',
+  'components.controls.image.byURL': 'URL',
+  'components.controls.image.dropFileText': 'Yüklemek için dosyayı sürükleyip bırakın ya da tıklayın',
+
+  // Inline
+  'components.controls.inline.bold': 'Kalın',
+  'components.controls.inline.italic': 'Italik',
+  'components.controls.inline.underline': 'Alt Çizgi',
+  'components.controls.inline.strikethrough': 'İçinden Geçen Çizgi',
+  'components.controls.inline.monospace': 'Monospace',
+  'components.controls.inline.superscript': 'Superscript',
+  'components.controls.inline.subscript': 'Subscript',
+
+  // Link
+  'components.controls.link.linkTitle': 'Link Başlığı',
+  'components.controls.link.linkTarget': 'Link Adresi',
+  'components.controls.link.linkTargetOption': 'Linki yeni sayfada aç',
+  'components.controls.link.link': 'Link',
+  'components.controls.link.unlink': 'Linki Kaldır',
+
+  // List
+  'components.controls.list.list': 'Liste',
+  'components.controls.list.unordered': 'Sıralı Değil',
+  'components.controls.list.ordered': 'Sıralı',
+  'components.controls.list.indent': 'Indent',
+  'components.controls.list.outdent': 'Outdent',
+
+  // Remove
+  'components.controls.remove.remove': 'Sil',
+
+  // TextAlign
+  'components.controls.textalign.textalign': 'Yazı Pozisyonu',
+  'components.controls.textalign.left': 'Sol',
+  'components.controls.textalign.center': 'Orta',
+  'components.controls.textalign.right': 'Sağ',
+  'components.controls.textalign.justify': 'Kenara yanaşmış',
+};
