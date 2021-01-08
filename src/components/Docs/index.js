b041847ac0584648cbdf157e1e2bc1ee305d7eb7
jpuri <jyotipuri@gmail.com>
2021-01-09 00:31:51 +0530

    Docs changes

--- src/components/Docs/index.js
+++ src/components/Docs/index.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import Installation from './Installation';
-import Usage from './Usage';
-import Props from './Props';
-import ARIASupport from './ARIASupport';
-import DataConversion from './DataConversion';
-import './styles.css';
+import React, { Component } from "react";
+import Installation from "./Installation";
+import Usage from "./Usage";
+import Props from "./Props";
+import ARIASupport from "./ARIASupport";
+import DataConversion from "./DataConversion";
+import "./styles.css";
 
-export default () =>
-<div className="docs-root">
-  <Installation />
-  <Usage />
-  <Props />
-  <ARIASupport />
-  <DataConversion />
-</div>;
+export default () => (
+  <div className="docs-root">
+    <Installation />
+    <Usage />
+    <Props />
+    <ARIASupport />
+    <DataConversion />
+  </div>
+);
