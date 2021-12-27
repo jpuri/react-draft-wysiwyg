31b7921e2fa959a3435f62d815c1ba0aa157c6bd
Guilherme Fischer <guilherme@gfischer.com.br>
2021-12-27 10:46:09 -0300

    feat ( dev ): Add dev support for editor

---
+++ local/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Editor from "../src/Editor";
+import "../css/Draft.css";
+
+ReactDOM.render(<Editor />, document.getElementById("root"));
