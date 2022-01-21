628206099b7747fda4a20b045a8f475d53c13c25
brahim smahi <brahim@myminifactory.com>
2022-01-21 16:20:37 +0100

    fix embedded youtube video

--- src/renderer/Embedded/index.js
+++ src/renderer/Embedded/index.js
@@ -4,7 +4,19 @@ import PropTypes from 'prop-types';
 const Embed = ({ block, contentState }) => {
   const entity = contentState.getEntity(block.getEntityAt(0));
   const { src, height, width } = entity.getData();
-  return (<iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" />);
+
+  const getId = (url) => {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const match = url.match(regExp);
+
+    return match && match[2].length === 11 ? match[2] : null;
+};
+
+const convertYTBUrl = (url: string) => {
+    return "https://www.youtube.com/embed/" + getId(url);
+};
+
+  return (<iframe height={height} width={width} src={convertYTBUrl(src)} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" />);
 };
 
 Embed.propTypes = {
